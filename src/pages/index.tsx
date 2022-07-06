b90dabcb530daaac2e189ebc3f2f39e5d0c02cf1
med <izekki.mohamed@gmail.com>
2022-07-06 20:03:49 +0200

    change dir

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="bg-slate-800 h-screen text-center p-4">
-      <h1 className="text-4xl text-indigo-600 tex-bold">
-        {" "}
-        This is a Blog post{" "}
-      </h1>
+      <h1 className="text-4xl text-indigo-600 tex-bold">This is a Blog post</h1>
     </div>
   );
 };
